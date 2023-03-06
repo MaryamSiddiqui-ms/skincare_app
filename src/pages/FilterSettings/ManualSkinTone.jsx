e039558b794f720ff4d54e5d35359ad408002167
= <=>
2023-03-06 23:52:40 +0500

    4th page

--- src/pages/FilterSettings/ManualSkinTone.jsx
+++ src/pages/FilterSettings/ManualSkinTone.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
-import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
-
+import {Button} from './../../components';
+import Grid from '@mui/material/Grid';
+import './FilterSettings.css'
 function ManualSkinTone() {
 
   return (
     <div style = {{position:'relative' , height:'100vh'}}>
       <h1>Select Your skin Type</h1>
       <FormControl>
-        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <FormLabel id="demo-radio-buttons-group-label"></FormLabel>
         <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
+            defaultValue="African American"
             name="radio-buttons-group"
         >
-            <FormControlLabel value="female" control={<Radio />} label="Female" />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <FormControlLabel value="White" control={<Radio />} label="White" />
+            <FormControlLabel value="Asian" control={<Radio />} label="Asian" />
+            <FormControlLabel value="African American" control={<Radio />} label="African American" />
+            <FormControlLabel value="American Indian Or Alaska Native" control={<Radio />} label="American Indian Or Alaska Native" />
+            <FormControlLabel value="Native Hawaiian Or Other Pacific Islander" control={<Radio />} label="Native Hawaiian Or Other Pacific Islander" />
+
         </RadioGroup>
-        
+
     </FormControl>
     <div className='setting_BTN'>
     <Grid container justifyContent="space-between" >
